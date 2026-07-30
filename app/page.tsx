ac1b6ece0fa93144021096ce51bb01af9a8d954f
Sharath Teja <sharathteja19@gmail.com>
2026-07-30 22:05:27 +0530

    logo

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,27 @@
 import Image from "next/image";
 import Navbar from "../components/Navbar";
 import Hero from "../components/Landing/Hero";
+
 export default function Home() {
   return (
     <main>
-      <div className="flex justify-between items-center px-8 py-4">
-        <h2 className="text-4xl font-semi-bold">AI resume </h2>
+      <header className="flex items-center justify-between px-8 py-4">
+        <div className="flex items-center gap-3">
+          <Image
+            src="/logo.png"
+            alt="AI Resume Logo"
+            width={60}
+            height={60}
+            priority
+            className="rounded-md object-contain"
+          />
+
+          <h1 className="text-3xl font-bold">AI Resume</h1>
+        </div>
+
         <Navbar />
-      </div>
+      </header>
+
       <Hero />
     </main>
   );
